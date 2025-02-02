369810f7c4b3b91202ecb0c9e5844a6cf873edde
Robert Will <robert.will@friday.de>
2025-02-02 15:59:03 +0100

    first draft layer-based lighting; just copy-paste from example code

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
@@ -3,3 +3,5 @@
 
 #define RGB_MATRIX_TIMEOUT (5*60*1000) // number of milliseconds to wait until rgb automatically turns off
 #define RGB_MATRIX_SLEEP               // turn off effects when suspended
+
+#define SPLIT_LAYER_STATE_ENABLE
