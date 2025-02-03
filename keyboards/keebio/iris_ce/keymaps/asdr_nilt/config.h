620f9796349541d9441ce3a2fe760c9fb33dafb0
Robert Will <robert.will@friday.de>
2025-02-03 19:34:13 +0100

    Timeout for backlights.

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
@@ -1,2 +1,5 @@
 // this value comes from my experience with Ergodox.
 #define TAPPING_TERM 300
+
+#define RGB_MATRIX_TIMEOUT (5*60*1000) // number of milliseconds to wait until rgb automatically turns off
+#define RGB_MATRIX_SLEEP               // turn off effects when suspended
