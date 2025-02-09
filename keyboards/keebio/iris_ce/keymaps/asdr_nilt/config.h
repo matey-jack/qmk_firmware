afa164ab31ac35082820053674894174a6975bd4
Robert Will <matey.jack@gmail.com>
2025-02-09 17:34:35 +0100

    enable permissive hold

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
@@ -1,5 +1,9 @@
 // this value comes from my experience with Ergodox.
-#define TAPPING_TERM 300
+#define TAPPING_TERM 250
+
+// Switch to Hold mode before tapping term, when other key is pressed _and_ released,
+// all before the tap/hold key is released.
+#define PERMISSIVE_HOLD
 
 #define RGB_MATRIX_TIMEOUT (5*60*1000) // number of milliseconds to wait until rgb automatically turns off
 #define RGB_MATRIX_SLEEP               // turn off effects when suspended
