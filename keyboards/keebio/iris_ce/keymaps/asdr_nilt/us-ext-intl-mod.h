35c34ccdc822e61e440e078d178e2ac27326ef82
Robert Will <matey.jack@gmail.com>
2025-02-09 23:43:01 +0100

    first try layout that relies on software layout for shift layer.

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/us-ext-intl-mod.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/us-ext-intl-mod.h
@@ -5,5 +5,14 @@
 
 #define KX_DASH  S(ALGR(US_MINS))
 
-#define KX_PIPE KC_BSLS
-#define KX_BSLS KC_PIPE
+// the software layout has <> swapped with ;:
+// we don't need to map ;: because the shift layer is done by the software layout,
+// but we need to map <> (less than, greater than).
+#define KX_LT KC_SCLN
+#define KX_GT KC_COLN
+
+// the software layout moved dead accents to AltGr layer.
+// corresponding live characters are in same position as US ANSI keyboard,
+// so I use those constants, not defining new ones.
+#define KX_DGRV ALGR(KC_GRV)
+#define KX_ACUT ALGR(KC_QUOT)
