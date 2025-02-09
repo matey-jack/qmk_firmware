34d12ba31574eb8855561b64d5d5c5a9b5e6f928
Robert Will <matey.jack@gmail.com>
2025-02-09 17:34:35 +0100

    enable permissive hold

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/config.h
@@ -1,2 +1,6 @@
 // this value comes from my experience with Ergodox.
 #define TAPPING_TERM 300
+
+// Switch to Hold mode before tapping term, when other key is pressed _and_ released,
+// all before the tap/hold key is released.
+#define PERMISSIVE_HOLD
