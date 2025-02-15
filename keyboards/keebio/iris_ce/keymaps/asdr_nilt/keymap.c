7ce8ea04d924c7919d17bef57e1b883b8af39c9e
Robert Will <matey.jack@gmail.com>
2025-02-15 15:23:23 +0100

    fix mappings for characters that moved in the software layout.

--- keyboards/keebio/iris_ce/keymaps/asdr_nilt/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/asdr_nilt/keymap.c
@@ -6,9 +6,12 @@
 #include "keymap_us_international_linux.h"
 #include "us-ext-intl-mod.h"
 
+// prev/next word shortcuts, works in almost all applications.
+// (This helps avoid pressing Ctrl modifier in addition to the layer toggle.)
 #define KC_PRWD  LCTL(KC_LEFT)
 #define KC_NXWD  LCTL(KC_RGHT)
 
+// layer toggles all have double duty.
 #define L1_Y     LT(1, KC_Y)
 #define L1_MINS  LT(1, KC_MINS)
 #define L1_ENT   LT(1, KC_ENT)
@@ -19,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // standard keyboard layer
     [0] = LAYOUT(
             L2_ESC , KC_1, KC_2, KC_3, KC_4, KC_5   ,                     KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-            KC_TAB , KC_Q, KC_W, KC_B, KC_F, US_QUOT,                     KC_Z, KC_K, KC_U, KC_O, KC_P, KC_PGUP,
+            KC_TAB , KC_Q, KC_W, KC_B, KC_F, KC_QUOT,                     KC_Z, KC_K, KC_U, KC_O, KC_P, KC_PGUP,
             KC_LSFT, KC_A, KC_S, KC_D, KC_R, KC_G   ,                     KC_H, KC_N, KC_I, KC_L, KC_T, KC_RSFT,
             KC_LCTL, L1_Y, KC_X, KC_C, KC_V, KC_SLSH, KC_LGUI,    L2_INS, KC_J, KC_M, KC_COMM, KC_DOT, L1_MINS, KC_PGDN,
                                      KC_LALT, KC_DEL, KC_SPC ,   L1_ENT, KC_E, KC_RCTL
@@ -29,19 +32,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Dead tilde for accents can be obtained with Shift+US_GRV as on any other keyboard.
     // I just mention it, because this and capital ÄÖÜ are the only characters that need pressing both Shift and AltGr.
     [1] = LAYOUT(
-            KC_NO  , KX_DGRV, KX_ACUT, US_SECT, US_EURO, US_CENT,                       US_DCIR, KC_PIPE, KC_LBRC, KC_RBRC, US_IQUE, US_SS  ,
+            KC_NO  , KX_DGRV, KX_ACUT, US_SECT, US_EURO, US_CENT,                       KX_CIRC, KC_PIPE, KC_LBRC, KC_RBRC, US_IQUE, US_SS  ,
             KC_NO  , KC_NO  , KC_PRWD, KC_UP  , KC_NXWD, KC_GRV ,                       US_DEG , KC_BSLS, KC_LCBR, KC_RCBR, KC_TILD, US_UDIA,
-            KC_LSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END ,                       KC_NO  , US_MUL , KC_LPRN, KC_RPRN, US_ODIA, KC_RSFT,
+            KC_LSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END ,                       KC_NO  , US_MUL , KX_LPRN, KX_RPRN, US_ODIA, KC_RSFT,
 			KC_LCTL, KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LGUI,     KC_TRNS, US_MICR, KC_EQL , KX_LT  , KX_GT  , KX_DASH, US_ADIA,
                                                 KC_NO  , KC_BSPC, KC_ENT ,     KC_SPC , KC_NO  , KC_RCTL
         ),
-    // function layer, like on a laptop.
+    // function layer, like on a laptop. Includes a 'live' AltGr key, so we can access all characters from software layout AltGr that don't have
+    // a direct mapping in our firmware AltGr layer. (Meant for rare characters and as workaround for mapping bugs.)
     [2] = LAYOUT(
             KC_TRNS, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                       KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , EE_CLR,
             KC_NO  , KC_F11 , KC_F12 , KC_NO  , KC_NO  , KC_NO  ,                       KC_NO  , RM_TOGG, RM_HUED, RM_SATD, RM_VALD, QK_BOOT,
             KC_LSFT, KC_MPRV, KC_MNXT, KC_NO  , KC_NO  , KC_NO  ,                       KC_NO  , RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, KC_NO  ,
 			KC_LCTL, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LGUI,     KC_TRNS, KC_NO  , KC_MUTE, KC_VOLD, KC_VOLU, KC_MSTP, KC_MPLY,
-                                                KC_NO  , KC_NO  , KC_NO  ,     KC_NO  , KC_NO  , KC_RCTL
+                                                KC_ALGR, KC_NO  , KC_NO  ,     KC_NO  , KC_NO  , KC_RCTL
         )
 };
 
