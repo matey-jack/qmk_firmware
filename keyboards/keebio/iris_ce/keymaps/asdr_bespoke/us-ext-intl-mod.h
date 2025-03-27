2e18c7932d076433c098f317ec3d9794dc4b10e5
Robert Will <matey.jack@gmail.com>
2025-03-27 23:26:03 +0100

    improve comments in Header file for the support layout.

--- keyboards/keebio/iris_ce/keymaps/asdr_bespoke/us-ext-intl-mod.h
+++ keyboards/keebio/iris_ce/keymaps/asdr_bespoke/us-ext-intl-mod.h
@@ -5,21 +5,22 @@
 // it places ' and " in the same position as US ANSI.
 // That makes the firmware layout also work with devices that only have US ANSI.
 
-#define KX_DASH  S(ALGR(US_MINS))
-
-// the software layout has <> swapped with ;:
-// we don't need to map ;: because the shift layer is done by the software layout,
+// The software layout has <> swapped with ;:
+// We don't need to map ;: because the shift layer is done by the software layout,
 // but we need to map <> (less than, greater than).
 #define KX_LT KC_SCLN
 #define KX_GT KC_COLN
 
-// the software layout moved dead accents to AltGr layer.
-// corresponding live characters are in same position as US ANSI keyboard,
-// so I use those constants, not defining new ones.
+// The software layout moved dead accents to AltGr layer.
 #define KX_DGRV ALGR(KC_GRV)
 #define KX_ACUT ALGR(KC_QUOT)
+// Corresponding live characters are in same position as US ANSI keyboard,
+// So KC_GRV and KC_QUOT are still valid.
 
 // ^() moved to AltGr layer to make space for +=?
 #define KX_CIRC ALGR(KC_6)
 #define KX_LPRN ALGR(KC_9)
 #define KX_RPRN ALGR(KC_0)
+
+// And one new character which isn't mapped in standard US intl.
+#define KX_DASH  S(ALGR(US_MINS))
