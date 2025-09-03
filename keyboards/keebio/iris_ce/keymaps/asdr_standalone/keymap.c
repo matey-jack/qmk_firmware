871e9a04f23f12875cc5e3b05100cf3440d56f50
Robert Will <matey.jack@gmail.com>
2025-09-03 10:41:09 +0200

    temporary mapping for the "für" Makro that should later reside on the one-shot accents layer

--- keyboards/keebio/iris_ce/keymaps/asdr_standalone/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/asdr_standalone/keymap.c
@@ -53,6 +53,7 @@ enum custom_keycodes {
     MX_VERS = SAFE_RANGE,
     MX_DASH,  // TODO: this is for n-dash, but not sure how to make that work on Android.
     MX_PMIL,  // TODO: per-mille sign
+    MX_FUER,  // Makro to avoid same-finger trigram (f¨ur)
     // Next three are for characters that need different key taps on Google Pixel and other devices.
     // Toggled by MX_TQM.
     MX_QUOT,
@@ -141,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // TODO: the "live" US_GRV, US_CIRC and US_TILD don't work on US ext. int'l. We should fix that with the US ext. int'l compat plus layout.
     [L_ALTGR] = LAYOUT(
             KC_NO  , US_DGRV, MX_ACUT, US_CENT, US_EURO, MX_PMIL,                       US_CIRC, KC_PIPE, KC_LBRC, KC_RBRC, US_SECT, KC_DEL ,
-            KC_NO  , KC_NO  , KC_PRWD, KC_UP  , KC_NXWD, KC_NO  ,                       US_DEG , KC_BSLS, KC_LCBR, KC_RCBR, US_TILD, US_MICR,
+            KC_NO  , KC_NO  , KC_PRWD, KC_UP  , KC_NXWD, MX_FUER,                       US_DEG , KC_BSLS, KC_LCBR, KC_RCBR, US_TILD, US_MICR,
             KC_LSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END ,                       US_GRV , US_NTIL, KC_LPRN, KC_RPRN, US_CCED, KC_RSFT,
 			KC_LCTL, KC_TRNS, KC_TOP , KC_PGUP, KC_PGDN, KC_BOTT, KC_LGUI,     KC_RGUI, US_MUL , KC_EQL , KC_LT  , KC_GT  , MX_DASH, C(KC_INS),
                                                 KC_LALT, KC_BSPC, KC_ENT ,     KC_SPC , KC_NO  , KC_RCTL
@@ -184,6 +185,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
+        case MX_FUER:
+            if (!record->event.pressed) return false;
+            tap_code(KC_F);
+            tap_code16(US_DIAE);  // 16 bits, because the keycode has the Shift bit set.
+            tap_code(KC_U);
+            tap_code(KC_R);
+            return false;
         case MX_QUOT:
             if (!record->event.pressed) return false;
             switch (current_quote_mode) {
